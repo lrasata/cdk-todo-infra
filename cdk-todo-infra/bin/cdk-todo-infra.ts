815e533f23fd91eeccd036da1384b80a211fb089
Liantsoa Rasata <rmliantsoa@gmail.com>
2026-03-24 16:53:51 +0100

    feat: cdk todo infra api stack

--- cdk-todo-infra/bin/cdk-todo-infra.ts
+++ cdk-todo-infra/bin/cdk-todo-infra.ts
@@ -1,20 +1,18 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { CdkTodoInfraStack } from '../lib/cdk-todo-infra-stack';
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { ApiStack } from '../lib/stacks/api-stack';
+import {getConfig} from "../lib/config/environment";
 
 const app = new cdk.App();
-new CdkTodoInfraStack(app, 'CdkTodoInfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const stage = process.env.STAGE ?? 'dev';
+const config = getConfig(stage);
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new ApiStack(app, `ApiStack-${stage}`, {
+    env: {
+        account: config.account,
+        region: config.region,
+    },
+    config,
 });
---
+++ cdk-todo-infra/lib/stacks/api-stack.ts
@@ -0,0 +1,85 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import * as path from 'path';
+import {EnvConfig} from "../config/environment";
+
+interface ApiStackProps extends cdk.StackProps {
+    config: EnvConfig;
+}
+
+export class ApiStack extends cdk.Stack {
+    public readonly apiUrl: string;
+
+    constructor(scope: Construct, id: string, props: ApiStackProps) {
+        super(scope, id, props);
+
+        // DynamoDB table
+        const table = new dynamodb.Table(this, 'TodosTable', {
+            tableName: `todos-${props.config.stage}`,
+            partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+            sortKey: { name: 'todoId', type: dynamodb.AttributeType.STRING },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+            removalPolicy: props.config.removalPolicy,
+        });
+
+        // Lambda function
+        const todoHandler = new lambda.NodejsFunction(this, 'TodoHandler', {
+            functionName: `todo-handler-${props.config.stage}`,
+            runtime: Runtime.NODEJS_22_X,
+            entry: path.join(__dirname, '../../../todo-backend/src/handler.ts'),
+            handler: 'handler',
+            environment: {
+                TABLE_NAME: table.tableName,
+            },
+            logGroup: new logs.LogGroup(this, 'TodoHandlerLogs', {
+                logGroupName: `/aws/lambda/todo-handler-${props.config.stage}`,
+                retention: props.config.stage === 'prod'
+                    ? logs.RetentionDays.ONE_MONTH
+                    : logs.RetentionDays.ONE_WEEK,
+                removalPolicy: props.config.removalPolicy,
+            }),
+            bundling: {
+                minify: props.config.stage === 'prod',
+                sourceMap: props.config.stage !== 'prod',
+            },
+        });
+
+        // Grant Lambda read/write access to DynamoDB
+        table.grantReadWriteData(todoHandler);
+
+        // API Gateway
+        const api = new apigateway.RestApi(this, 'TodosApi', {
+            restApiName: `todos-api-${props.config.stage}`,
+            defaultCorsPreflightOptions: {
+                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                allowMethods: apigateway.Cors.ALL_METHODS,
+                allowHeaders: ['Content-Type', 'X-User-Id'],
+            },
+            deployOptions: {
+                stageName: props.config.stage,
+            },
+        });
+
+        const lambdaIntegration = new apigateway.LambdaIntegration(todoHandler);
+
+        const todos = api.root.addResource('todos');
+        todos.addMethod('GET', lambdaIntegration);
+        todos.addMethod('POST', lambdaIntegration);
+
+        const todo = todos.addResource('{todoId}');
+        todo.addMethod('PATCH', lambdaIntegration);
+        todo.addMethod('DELETE', lambdaIntegration);
+
+        this.apiUrl = api.url;
+
+        new cdk.CfnOutput(this, 'ApiUrl', {
+            value: api.url,
+            exportName: `TodoApiUrl-${props.config.stage}`,
+        });
+    }
+}
